b8eea11a109644b37fa31f96a2a7548c27c43ab8
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2025-01-24 12:47:43 +0000

    Soon...

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addlayer("s", {
+addLayer("s", {
     name: "subscribers", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -69,7 +69,15 @@ addlayer("s", {
             cost: new Decimal (250),
             unlocked () {return (hasUpgrade(this.layer, 13))}},
         22: {
-            title: "Start coding CodeMark TradeName"
+            title: "Start coding CodeMark TradeName",
+            cost: new Decimal (1000),
+            description: "Just a small boost, boost Subs based on Subs. Not inflated yet I swear (x^0.2)",
+            unlocked() {return (hasUpgrade(this.layer, 13))},
+            effect() {
+                let ret = player[this.layer].points.pow(0.2)
+                if (ret.gte("1e10")) ret = ret.pow(0.4).times("1e6")
+                return ret
+            }
         }
         }
     })
