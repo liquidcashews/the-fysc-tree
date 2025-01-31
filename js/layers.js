ea833708e2014bf3068d35c911dd879b9535babc
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2025-01-31 13:59:57 +0000

    hm

--- js/layers.js
+++ js/layers.js
@@ -22,9 +22,9 @@ addLayer("s", {
         return mult
     },
     gainExp()
-    {let ret = new Decimal (1)
-        if (hasUpgrade(this.layer, 21)) ret = ret.times(1.2)
-            return ret
+    {
+    if (hasUpgrade(this.layer, 21)) return new Decimal (1.2)
+        return new Decimal (1)
     },
     hotkeys: [
         {key: "s", description: "s: Reset for subs", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
