e557c14baad0e82dae59fabe0e19daae321c6bdd
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2025-03-04 03:55:02 +0000

    low taper fade

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("subscribe", {
+addLayer("subs", {
     name: "subscribers", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     row: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -12,18 +12,18 @@ addLayer("subscribe", {
     baseResource: "fysc players", // Name of resource prestige is based on
     exponent: 0.5,
     position: 0,
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.points; }, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade(this.layer, 11)) mult = mult.times(2)
-        if (hasUpgrade(this.layer, 22)) mult = mult.times(upgradeEffect(this.layer, 22))
+        if (hasUpgrade("subs", 11)) mult = mult.times(2)
+        if (hasUpgrade("subs", 22)) mult = mult.times(upgradeEffect(this.layer, 22))
         return mult
     },
     gainExp()
     {
         sigma = new Decimal (1)
-    if (hasUpgrade(this.layer, 21)) sigma = new Decimal (1.2)
+    if (hasUpgrade("subs", 21)) sigma = new Decimal (1.2)
         return sigma
     },
     hotkeys: [
@@ -42,9 +42,9 @@ addLayer("subscribe", {
             title: "Create a forum in the FYSC Discord",
             description: "Boost Fysc Players based on subs. (x^0.45)",
             cost: new Decimal(10),
-            unlocked() {return (hasUpgrade(this.layer, 11))},
+            unlocked() {return (hasUpgrade("subs", 11))},
             effect() {
-                let ret = player.subscribe.points.add(1).pow(0.45)
+                let ret = player.subs.points.add(1).pow(0.45)
                 if (ret.gte("1000000")) ret = ret.sqrt().times("1000")
                     return ret;
             },
@@ -54,7 +54,7 @@ addLayer("subscribe", {
             title: "Just Self-Synergy",
             description: "Boost FP based on FP. (x^0.35)",
             cost: new Decimal (100),
-            unlocked() {return (hasUpgrade(this.layer, 12))},
+            unlocked() {return (hasUpgrade("subs", 12))},
             effect() {
                 let ret = player.points.add(1).pow(0.35)
                 if (ret.gte("1e30")) ret = ret.pow(0.2).times("1e24")
@@ -69,16 +69,16 @@ addLayer("subscribe", {
             effect()
             {return new Decimal (1.2)
             },
-            unlocked () {return (hasUpgrade(this.layer, 13))},
+            unlocked () {return (hasUpgrade("subs", 13))},
             effectDisplay () {return format (this.effect())+"x"},
     },
         22: {
             title: "Start coding CodeMark TradeName",
             cost: new Decimal (1000),
             description: "Just a small boost, boost Subs based on Subs. Not inflated yet I swear (log3)",
-            unlocked() {return (hasUpgrade(this.layer, 21))},
+            unlocked() {return (hasUpgrade("subs", 21))},
             effect() {
-                let ret = player.subscribe.points.add(1).log(3)
+                let ret = player.subs.points.add(1).log(3)
                 return ret
             },
              effectDisplay () {return format (this.effect())+"x"},
@@ -91,17 +91,17 @@ addLayer("subscribe", {
     row: 1,
     position: 0,
     startData() { return {
-        unlocked: (hasUpgrade("s", 22)),
+        unlocked: (hasUpgrade("subs", 22)),
         points: new Decimal (0),
     }},
-    baseAmount() {return player.subscribe.points},
+    baseAmount() {return player.subscribe.points; },
     color: "#011F98", // color stat wars gave me
     exponent: 0.3, // player[layer] my taper
     type: "normal",
     resource: "HEXZD points",
     baseResource: "subscribers",
     softcapPower: new Decimal (0.4),
-    layerShown() {return (hasUpgrade ("subscribe", 22))},
+    layerShown() {return (hasUpgrade ("subs", 22))},
     gainMult() {
         return new Decimal (1)
     },
