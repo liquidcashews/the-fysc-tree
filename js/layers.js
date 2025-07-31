4b7a3729bdcc8a461a6df093775d13610dd31fdc
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2025-07-31 07:20:11 +0000

    Idk if this is correct tbh

--- js/layers.js
+++ js/layers.js
@@ -18,7 +18,7 @@ addLayer("subs", {
         mult = new Decimal(1)
         if (hasUpgrade("subs", 11)) mult = mult.times(2)
         if (hasUpgrade("subs", 22)) mult = mult.times(upgradeEffect(this.layer, 22))
-        mult = mult.times(eff.subinflation)
+        mult = mult.times(effect(subinflation))
         return mult
     },
     gainExp()
