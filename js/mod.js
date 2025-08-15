24359bf8d3ce82496bc123ba93339f24955c587c
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2025-08-15 09:12:56 +0000

    s

--- js/mod.js
+++ js/mod.js
@@ -12,8 +12,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.12",
-	name: "hexzd upgrades",
+	num: "0.121",
+	name: "hexzd upgrades again",
 }
 
 let changelog = `<h1>Changelog:</h1><br>,
