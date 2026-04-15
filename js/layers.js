d8037637f1978f05dd914970a7741a4a045c12b8
liquidcashews <152039932+liquidcashews@users.noreply.github.com>
2026-04-15 08:41:04 +0800

    this is gonna break

--- js/layers.js
+++ js/layers.js
@@ -23,7 +23,7 @@ addLayer("subs", {
         if (hasUpgrade("hexzd", 14)) mult = mult.times(upgradeEffect("hexzd", 14))
         if (hasMilestone("ct", 0)) mult = mult.times(3)
 		if (hasMilestone("ct", 1)) mult = mult.times(tmp.hexzd.effect.inflation)
-        mult = mult.times(tmp.ct.effect.ctrecovery)
+        mult = mult.times(tmp[ct].effect.ctrecovery)
         return mult
     },
     gainExp()
@@ -172,7 +172,7 @@ addLayer("hexzd", {
         let yum = new Decimal (1)
         if (hasUpgrade("hexzd", 15)) yum = yum.times(upgradeEffect("hexzd", 15))
         if (hasUpgrade("hexzd", 24)) yum = yum.times(upgradeEffect(this.layer, 24))
-        yum = yum.times(tmp.ct.effect.ctrecovery)
+        yum = yum.times(tmp[ct].effect.ctrecovery)
         if (hasMilestone("ct", 0)) yum = yum.times(2)
         return yum
     },
